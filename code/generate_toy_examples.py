0d5785b15df9a8a76aaa9b6565f5ce7972d7e50f
yurkovak <katedayreen@gmail.com>
2023-03-14 00:57:30 +0200

    feature: Synthetic data col & row subsampling

--- code/generate_toy_examples.py
+++ code/generate_toy_examples.py
@@ -60,19 +60,22 @@ class ToyGenerator:
         verts, _, _ = load_obj(os.path.join(self.data_path, 'points', f'{scene_name}.obj'))
         return verts.numpy()
 
-    def generate(self, scene_name: str, cameras_src_name: str, visualize: bool = True, target_fraction=None):
+    def generate(self, scene_name: str, cameras_src_name: str, visualize: bool = True, target_fraction: float = None,
+                 column_subsample: int = None, row_subsample: int = None):
         if scene_name == 'line':
             world_points = self._get_line_points()
         else:
             world_points = self._get_obj_points(scene_name)
 
-        scene_data = self._get_scene_data(world_points, cameras_src_name, target_fraction)
+        scene_data = self._get_scene_data(world_points, cameras_src_name, target_fraction,
+                                          column_subsample, row_subsample)
 
-        np.savez(os.path.join(self.data_path, f'{scene_name} [CAM {cameras_src_name}].npz'), **scene_data)
+        np.savez(os.path.join(self.data_path, f'{scene_name} [CAM {cameras_src_name}]_small.npz'), **scene_data)
         if visualize:
             self.visualize_data(scene_data, scene_name, cameras_src_name)
 
-    def _get_scene_data(self, world_points: np.ndarray, cameras_src_name: str, target_fraction: tuple = None) -> dict:
+    def _get_scene_data(self, world_points: np.ndarray, cameras_src_name: str, target_fraction: tuple = None,
+                        column_subsample: int = None, row_subsample: int = None) -> dict:
         scene_data = {}
         camera_cfg = CAMERA_SRC_CFG['scenes'][cameras_src_name]
 
@@ -94,10 +97,20 @@ class ToyGenerator:
         uvz = uvz[:, :2]
         m, _, n = uvz.shape
         M = uvz.reshape(m * 2, n)
+
+        M = geo_utils.remove_empty_tracks_cams(M)
+
+        if column_subsample != None:
+            sampled_cols = np.sort(np.random.choice(n, size=min(column_subsample, n), replace=False))
+            M = M[:, sampled_cols]
+        if row_subsample != None:
+            sampled_rows = np.sort(np.random.choice(m, size=min(row_subsample, m), replace=False))
+            M = M[np.sort(np.concatenate([sampled_rows * 2, sampled_rows * 2 + 1]))]
+
         if target_fraction is not None:
             M = geo_utils.sparsify_M(M, target_fraction)
 
-        scene_data['M'] = geo_utils.remove_empty_tracks_cams(M)
+        scene_data['M'] = M
         return scene_data
 
     @staticmethod
@@ -159,6 +172,10 @@ if __name__ == '__main__':
     parser.add_argument('--target_fraction', '-frac', default=None, type=str,
                         help='Sampling range for sparsification (how much should stay) given as a space-separated '
                              'string e.g. "0.07 0.4"')
+    parser.add_argument('--column_subsample', '-colsub', default=None, type=int,
+                        help='Num columns (tracks) in the resulting data')
+    parser.add_argument('--row_subsample', '-rowsub', default=None, type=int,
+                        help='Num rows (cameras) in the resulting data')
     args = parser.parse_args()
 
     if args.target_fraction is not None:
@@ -169,4 +186,5 @@ if __name__ == '__main__':
     for scene_name in scene_names:
         for camera_src in camera_srcs:
             print(f'Generating {scene_name} with cameras from {camera_src}...')
-            generator.generate(scene_name, camera_src, visualize=args.visualize, target_fraction=args.target_fraction)
+            generator.generate(scene_name, camera_src, visualize=args.visualize, target_fraction=args.target_fraction,
+                               column_subsample=args.column_subsample, row_subsample=args.row_subsample)
